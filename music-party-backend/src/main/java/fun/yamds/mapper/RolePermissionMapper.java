c2ba07cf4f0860e66ddf30df2472d51d72eb60bf
Yamds <yamds@qq.com>
2025-05-19 15:49:31 +0800

    fix: register

---
+++ music-party-backend/src/main/java/fun/yamds/mapper/RolePermissionMapper.java
@@ -0,0 +1,9 @@
+package fun.yamds.mapper;
+
+/**
+ * @author     ：Yamds
+ * @createtime       ：2025/5/19
+ * @description：RolePermissionMapper
+ */
+public interface RolePermissionMapper {
+}
---
+++ music-party-backend/src/main/java/fun/yamds/pojo/RolePermissionPojo.java
@@ -0,0 +1,9 @@
+package fun.yamds.pojo;
+
+/**
+ * @author     ：Yamds
+ * @createtime       ：2025/5/19
+ * @description：RolePermissionPojo
+ */
+public class RolePermissionPojo {
+}
